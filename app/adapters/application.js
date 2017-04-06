84656219107ed695ffc71ab3f7d9c7300b797b00
Artem Harmaty <harmaty@gmail.com>
2017-04-06 15:31:35 +0300

    localhost comment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   host: 'http://secret-lake-72236.herokuapp.com',
+  //host: 'http://localhost:3000',
   namespace: 'api/v1',
 
   pathForType: function(type) {
